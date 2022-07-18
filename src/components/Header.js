1f79155816468e713ce8da03ae181e3d08b51054
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-07-18 15:29:25 -0300

    react: finishing header searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
 
-function Header({ title, hasSearchBar }) {
+function Header({ title, hasSearchBar, history }) {
+  const [searchBarVisible, setSearchbar] = useState(false);
+
   return (
     <>
-      <img src={ profileIcon } alt="profile icon" data-testid="profile-top-btn" />
+      <button
+        type="button"
+        onClick={ () => history.push('/profile') }
+      >
+        <img
+          src={ profileIcon }
+          alt="profile icon"
+          data-testid="profile-top-btn"
+        />
+      </button>
       {hasSearchBar && (
-        <img src={ searchIcon } alt="search icon" data-testid="search-top-btn" />
+        <button
+          type="button"
+          onClick={ () => setSearchbar(!searchBarVisible) }
+        >
+          <img
+            src={ searchIcon }
+            alt="search icon"
+            data-testid="search-top-btn"
+          />
+        </button>
       )}
       <h1 data-testid="page-title">{title}</h1>
-      <SearchBar />
+      {searchBarVisible && <SearchBar />}
     </>
   );
 }
@@ -20,6 +40,11 @@ function Header({ title, hasSearchBar }) {
 Header.propTypes = {
   title: PropTypes.string.isRequired,
   hasSearchBar: PropTypes.bool.isRequired,
+  history: PropTypes.shape(),
+};
+
+Header.defaultProps = {
+  history: {},
 };
 
 export default Header;
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-function Recipes() {
+function Recipes({ history }) {
   return (
     <>
-      <Header title="Foods" hasSearchBar />
+      <Header title="Foods" hasSearchBar history={ history } />
       <Footer />
     </>
   );
 }
+
+Recipes.propTypes = {
+  history: PropTypes.shape(),
+};
+
+Recipes.defaultProps = {
+  history: {},
+};
 
 export default Recipes;
