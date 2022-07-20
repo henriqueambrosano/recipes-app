caa55b6cf1e254029c5984d491d12302e9b1778a
Joao Otavio Gomes Hugill <joao.gomeshugill@gmail.com>
2022-07-20 19:01:35 -0300

    javascript: requisito 37 até o 40

--- src/components/DrinkDetailCard.js
+++ src/components/DrinkDetailCard.js
@@ -1,14 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import copy from 'clipboard-copy';
-import shareIcon from '../images/shareIcon.svg';
-import whiteHeart from '../images/whiteHeartIcon.svg';
-import blackHeart from '../images/blackHeartIcon.svg';
+import ShareBtn from './ShareBtn';
+import FavoriteBtn from './FavoriteBtn';
 
 const DrinkDetailCard = ({ cardDetails, path }) => {
-  const [copied, setCopied] = useState('');
   const { strDrinkThumb, strDrink, strInstructions, strAlcoholic } = cardDetails;
-  const [favoriteRecipe, setFavoriteRecipe] = useState(false);
 
   const ingredientsKeys = Object.keys(cardDetails)
     .filter((item) => item.includes('Ingredient'));
@@ -19,51 +15,6 @@ const DrinkDetailCard = ({ cardDetails, path }) => {
   const measures = measuresKeys.map((item) => cardDetails[item])
     .filter((item) => item !== null);
 
-  const checkFavorite = () => {
-    if (localStorage.getItem('favoriteRecipes')) {
-      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-      const isFavorite = favoriteRecipes.some((item) => item.id === cardDetails.idDrink);
-      setFavoriteRecipe(isFavorite);
-    }
-  };
-
-  useEffect(() => {
-    checkFavorite();
-  }, []);
-
-  const copyToClipboard = () => {
-    setCopied('Link copied!');
-    copy(`http://localhost:3000${path}`);
-  };
-
-  const setAsFavorite = () => {
-    const newFavorite = {
-      id: cardDetails.idDrink,
-      type: 'drink',
-      nationality: '',
-      category: cardDetails.strCategory,
-      alcoholicOrNot: cardDetails.strAlcoholic,
-      name: cardDetails.strDrink,
-      image: cardDetails.strDrinkThumb,
-    };
-    if (localStorage.getItem('favoriteRecipes')) {
-      const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-      const isFavorite = favoriteRecipes.some((item) => item.id === cardDetails.idDrink);
-      if (isFavorite) {
-        const newFavorites = favoriteRecipes
-          .filter((recipe) => recipe.id !== cardDetails.idDrink);
-        localStorage.setItem('favoriteRecipes', JSON.stringify(newFavorites));
-      } else {
-        localStorage
-          .setItem('favoriteRecipes', JSON.stringify([...favoriteRecipes, newFavorite]));
-      }
-    } else {
-      localStorage
-        .setItem('favoriteRecipes', JSON.stringify([newFavorite]));
-    }
-    checkFavorite();
-  };
-
   return (
     <>
       <img
@@ -73,20 +24,8 @@ const DrinkDetailCard = ({ cardDetails, path }) => {
         className="recipe-image"
       />
       <h1 data-testid="recipe-title">{strDrink}</h1>
-      <button type="button" data-testid="share-btn" onClick={ copyToClipboard }>
-        <img src={ shareIcon } alt="share-btn" />
-      </button>
-      <span>{copied}</span>
-      <button
-        type="button"
-        onClick={ setAsFavorite }
-      >
-        <img
-          data-testid="favorite-btn"
-          src={ favoriteRecipe ? blackHeart : whiteHeart }
-          alt="favorite-btn"
-        />
-      </button>
+      <ShareBtn path={ path } />
+      <FavoriteBtn cardDetails={ cardDetails } recipeType="idDrink" />
       <p data-testid="recipe-category">{strAlcoholic}</p>
       {ingredients.map((ingr, i) => (
         <p
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,11 +1,132 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import FavoriteBtn from '../components/FavoriteBtn';
+import ShareBtn from '../components/ShareBtn';
+import fetchRecipes from '../services/services';
+import { isItDrinkF } from '../services/helpers';
 
-function RecipeInProgress() {
+function RecipeInProgress({ props: { history, match }, title }) {
+  const [detailsRecipe, setDetailsRecipe] = useState({});
+  const [ingredientsFound, setIngredientsFound] = useState([]);
+  const [measuresFound, setMeasuresFound] = useState([]);
+
+  const thumbNail = title === 'Drinks' ? 'Drink' : 'Meal';
+  const enterData = title === 'Drinks' ? 'drinks' : 'meals';
+  const recipeType = title === 'Drinks' ? 'cocktails' : 'meals';
+  const initialLocalStorage = (ingredient) => (
+    title === 'Drinks'
+      ? { cocktails: { [match.params.id]: [ingredient] }, meals: {} }
+      : { cocktails: { }, meals: { [match.params.id]: [ingredient] } }
+  );
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      const details = await fetchRecipes(
+        `https://www.${isItDrinkF(title)}.com/api/json/v1/1/lookup.php?i=${
+          match.params.id
+        }`,
+      );
+      setDetailsRecipe(details[enterData][0]);
+    };
+    fetchDetails();
+  }, []);
+
+  useEffect(() => {
+    const ingredientsKeys = Object.keys(detailsRecipe)
+      .filter((item) => item.includes('Ingredient'));
+    const measuresKeys = Object.keys(detailsRecipe)
+      .filter((item) => item.includes('Measure'));
+    const ingredients = ingredientsKeys
+      .map((item) => detailsRecipe[item])
+      .filter((item) => item !== '' && item !== null);
+    const measures = measuresKeys
+      .map((item) => detailsRecipe[item])
+      .filter((item) => item !== '' && item !== null);
+    setIngredientsFound(ingredients);
+    setMeasuresFound(measures);
+  }, [detailsRecipe]);
+
+  const addIngredient = (ingredient) => {
+    if (!localStorage.getItem('inProgressRecipes')) {
+      localStorage.setItem(
+        'inProgressRecipes',
+        JSON.stringify(initialLocalStorage(ingredient)),
+      );
+    } else {
+      const recipesFound = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      let newIngredients;
+      const isAlreadyInside = recipesFound[recipeType][match.params.id]
+        .some((savedIngredients) => savedIngredients === ingredient);
+
+      if (isAlreadyInside) {
+        newIngredients = recipesFound[recipeType][match.params.id]
+          .filter((ingredientSaved) => ingredientSaved !== ingredient);
+      } else {
+        newIngredients = [...recipesFound[recipeType][match.params.id], ingredient];
+      }
+      const finalObject = {
+        ...recipesFound,
+        [recipeType]: {
+          ...recipesFound[recipeType],
+          [match.params.id]: newIngredients,
+        },
+      };
+      localStorage.setItem('inProgressRecipes', JSON.stringify(finalObject));
+    }
+  };
+
+  const checkDisabled = (ingredient) => {
+    if (localStorage.getItem('inProgressRecipes')) {
+      const recipesFound = JSON.parse(localStorage.getItem('inProgressRecipes'));
+      return recipesFound[recipeType][match.params.id]
+        .some((savedIngredients) => savedIngredients === ingredient);
+    }
+    return false;
+  };
+
   return (
-    <>
-      Conteudo
-    </>
+    <div>
+      {Object.keys(detailsRecipe).length && (
+        <>
+          <img
+            className="recipe-image"
+            src={ `${detailsRecipe[`str${thumbNail}Thumb`]}` }
+            alt=""
+            data-testid="recipe-photo"
+          />
+          <h2 data-testid="recipe-title">{ detailsRecipe[`str${thumbNail}`] }</h2>
+          <ShareBtn path={ history.location.pathname } />
+          <FavoriteBtn
+            cardDetails=""
+            recipeType={ title === 'Drinks' ? 'idDrink' : 'idMeal' }
+          />
+          <p data-testid="recipe-category">{detailsRecipe.strCategory}</p>
+          {ingredientsFound.map((ingredient, index) => (
+            <label
+              data-testid={ `${index}-ingredient-step` }
+              htmlFor={ `ingredientFound${index}` }
+              key={ `ingredient${index}` }
+            >
+              <input
+                id={ `ingredientFound${index}` }
+                type="checkbox"
+                onClick={ () => addIngredient(ingredient) }
+                defaultChecked={ checkDisabled(ingredient) }
+              />
+              {`${ingredient} - ${measuresFound[index]}`}
+            </label>
+          ))}
+          <p data-testid="instructions">{detailsRecipe.strInstructions}</p>
+          <button type="button" data-testid="finish-recipe-btn">Finalizar receita</button>
+        </>
+      )}
+    </div>
   );
 }
+
+RecipeInProgress.propTypes = {
+  title: PropTypes.string.isRequired,
+  props: PropTypes.shape().isRequired,
+};
 
 export default RecipeInProgress;
