e5321e023ed815c8485299e6b4e7b9a5d5183568
Batista <jorge.lsbatista@gmail.com>
2022-07-21 08:39:18 -0300

    pequenos ajustes para dar pull e mergear com os arquivos mais recentes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,39 +5,41 @@ import Footer from '../components/Footer';
 
 function Profile({ history }) {
   return (
-    <>
+    <main>
       <Header title="Profile" hasSearchBar={ false } history={ history } />
+      <section>
+        <label htmlFor="email">
+          User:
+          <input
+            type="email"
+            data-testid="profile-email"
+            name="email"
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/done-recipes') }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/favorite-recipes') }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => history.push('/') }
+        >
+          Logout
+        </button>
+      </section>
       <Footer history={ history } />
-      <label htmlFor="email">
-        User:
-        <input
-          type="email"
-          data-testid="profile-email"
-          name="email"
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => history.push('/done-recipes') }
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => history.push('/favorite-recipes') }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ () => history.push('/') }
-      >
-        Logout
-      </button>
-    </>
+    </main>
   );
 }
 
